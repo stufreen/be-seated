7fa56c753c68536aa19cc383ccae1c2cd520eb39
Stu <stu@pilotinteractive.ca>
2019-09-22 13:00:23 -0400

    Add interstate font

--- webpack.common.js
+++ webpack.common.js
@@ -16,18 +16,22 @@ module.exports = {
   module: {
     rules: [
       {test: /\.json$/, loader: "json-loader"},
-
       {
         loader: "babel-loader",
         test: /\.js?$/,
         exclude: /node_modules/,
         query: {cacheDirectory: true}
       },
-
       {
         test: /\.(sa|sc|c)ss$/,
         exclude: /node_modules/,
         use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          "file-loader"
+        ]
       }
     ]
   },
@@ -36,19 +40,10 @@ module.exports = {
     new webpack.ProvidePlugin({
       fetch: "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
     }),
-
     new AssetsPlugin({
       filename: "webpack.json",
       path: path.join(process.cwd(), "site/data"),
       prettyPrint: true
     }),
-
-    new CopyWebpackPlugin([
-      {
-        from: "./src/fonts/",
-        to: "fonts/",
-        flatten: true
-      }
-    ])
   ]
 };
