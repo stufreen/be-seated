fd3b9209658ec1bfb10e23228a317e9cde2640a5
Stu <stu@pilotinteractive.ca>
2019-09-24 19:09:21 -0400

    Fix broken js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import {Application} from "stimulus";
-import {definitionsFromContext} from "stimulus/webpack-helpers";
+// import {Application} from "stimulus";
+// import {definitionsFromContext} from "stimulus/webpack-helpers";
 import "./css/main.scss";
 
-const application = Application.start();
-const context = require.context("./controllers", true, /\.js$/);
-application.load(definitionsFromContext(context));
+// const application = Application.start();
+// const context = require.context("./controllers", true, /\.js$/)
+// application.load(definitionsFromContext(context));
