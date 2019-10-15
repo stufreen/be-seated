5150e4b2287b78adb543fc59ff30359936359359
Stu Freen <stu.freen@shopify.com>
2019-10-15 09:33:55 -0400

    Animate the mobile menu

--- src/js/mobile-menu-drawer.js
+++ src/js/mobile-menu-drawer.js
@@ -1,11 +1,26 @@
+import * as animol from "animol";
+
 export default function initializeMobileMenu() {
   const opener = document.getElementById("mobile-menu-opener");
   const closer = document.getElementById("mobile-menu-closer");
   const drawer = document.getElementById("mobile-menu-drawer");
+  const animatedItems = document.getElementsByClassName("bs_mobileMenu_animatedItem");
 
   opener.addEventListener("click", () => {
     drawer.classList.add("open");
     document.body.classList.add("mobile-drawer-open");
+
+    for (let i = 0; i < animatedItems.length; i++) {
+      animatedItems[i].style.opacity = 0;
+      animol.css(
+        animatedItems[i],
+        300,
+        {transform: {translateX: "-0.5rem", rotateX: "10deg"}, opacity: 0},
+        {transform: {translateX: "0rem", rotateX: "0deg"}, opacity: 1},
+        animol.Easing.easeOutCubic,
+        30 * i
+      );
+    }
   });
 
   closer.addEventListener("click", () => {
